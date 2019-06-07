6ae7806e466bcb40bc00f2165e37b4bf0407730f
Naif5638 <51485239+Naif5638@users.noreply.github.com>
2019-06-07 10:53:52 -0400

    Creacion de Proyectos

---
+++ PruebaConsolaGit/PruebaConsolaGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PruebaConsolaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Probando GitHub");
+        }
+    }
+}
